fb9608effc1a544241aae5cc5f1e5b8a050823e1
Ilya <ilya.shuvatov1@gmail.com>
2022-11-08 07:55:51 +0100

    Initial commit

---
+++ js.js
@@ -0,0 +1,21 @@
+// JavaScript-код с созданием переменных
+
+
+
+/*
+Автор:
+Илья Шуватов.
+*/
+
+
+let myName = 'Ilya';
+let mySurname = 'Shuvatov';
+let myFavoriteDrink = 'Coffee';
+let myFavoriteAnimal = 'Dog';
+let myFavoriteProgrammingLanguage = 'JS'
+
+console.log(myName)
+console.log(mySurname)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myFavoriteProgrammingLanguage)
